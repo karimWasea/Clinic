56c8d706e3c206c8b553740353cf4b2a0f8aba8a
karim <karim@DESKTOP-KM53IJE>
2024-01-25 01:20:13 +0200

    lookpChanges

--- Clinic.Abstract/IlookupServess.cs
+++ Clinic.Abstract/IlookupServess.cs
@@ -1,7 +1,10 @@
-﻿namespace Clinic.Abstract
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Clinic.Abstract
 {
     public interface IlookupServess
     {
+        public List<SelectListItem> AvailableAppointments(Guid doctorId, int chosenDayOffset = 5);
 
     }
 }
--- Clinic.Service/lookupServess.cs
+++ Clinic.Service/lookupServess.cs
@@ -2,14 +2,75 @@
 
 using Clinic.Abstract;
 
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 namespace Clinic.Service
 {
     public class lookupServess : IlookupServess
     {
 
-        public readonly ApplicationDBcontext _context;
- 
-        public lookupServess() { }
- 
+        public readonly ApplicationDBcontext _applicationDBcontext;
+
+        public lookupServess(ApplicationDBcontext _applicationDBcontext)
+        {
+
+            this._applicationDBcontext = _applicationDBcontext;
+
+
+        }
+
+
+
+
+
+
+
+        public List<SelectListItem> AvailableAppointments(Guid doctorId, int chosenDayOffset = 5)
+        {
+            var doctorExistingAppointments = new HashSet<DateTime>(
+                _applicationDBcontext.Visits
+                    .Where(da => da.Id == doctorId)
+                    .Select(da => da.VisitsApientment));
+
+            var availableSlots = new List<SelectListItem>();
+
+            DateTime currentDate = DateTime.Now.Date;
+            DateTime futureDate = currentDate.AddDays(7);
+            DateTime startOfDay = futureDate.Date.AddHours(12); // Start time for appointments
+            DateTime endOfDay = futureDate.Date.AddHours(12 + 23).AddMinutes(30 * 23); // End time for appointments
+
+
+            while (currentDate <= futureDate)
+            {
+                //for (int i = 0; i < 24; i++)
+                //{
+                for (DateTime potentialTimeSlot = startOfDay; potentialTimeSlot <= endOfDay; potentialTimeSlot = potentialTimeSlot.AddMinutes(30))
+                {
+
+                    if (!IsTimeSlotBooked(doctorId, potentialTimeSlot))
+                    {
+                        if (potentialTimeSlot > DateTime.Now)
+                        {
+                            availableSlots.Add(new SelectListItem
+                            {
+                                Value = potentialTimeSlot.ToString("yyyy-MM-dd HH:mm"),
+                                Text = potentialTimeSlot.ToString("yyyy-MM-dd hh:mm tt")
+                            });
+                        }
+                    }
+                }
+
+                currentDate = currentDate.AddDays(1);
+            }
+
+            return availableSlots;
+        }
+
+        private bool IsTimeSlotBooked(Guid doctorId, DateTime timeSlot)
+        {
+            return _applicationDBcontext.Visits
+                .Any(da => da.Id == doctorId && da.VisitsApientment == timeSlot);
+        }
+
     }
 }

fbce7261ca0efe63f56a944d626492c4ac8b4d78
karim <karim@DESKTOP-KM53IJE>
2024-01-27 03:32:30 +0200

    all

---
+++ Utalieties/DbInitializers.cs
@@ -0,0 +1,64 @@
+﻿using Clincic.DataAccesslayer;
+
+using Clinic.Abstract;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utalieties
+{
+    public class DbInitializers : IDbInitializer
+    {
+        private readonly ApplicationDBcontext _context;
+
+        public DbInitializers(ApplicationDBcontext context)
+        {
+            _context = context;
+        }
+
+        public void Initialize()
+        {
+            try
+            {
+                if (_context.Database.GetAppliedMigrations().Count() > 0)
+                    _context.Database.Migrate();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            if (!_context.Clinic.Any())
+            {
+                var clinicData = new List<(string clinicName, int numberOfDoctors, bool isOpen, string location)>
+                {
+                    ("Example Clinic 1", 5, true, "Location 1"),
+                    ("Example Clinic 2", 3, true, "Location 2"),
+                    // Add more clinic data as needed
+                };
+
+                var clinics = new List<Clincic.DataAccesslayer.Clinic>();
+                foreach (var clinic in clinicData)
+                {
+                    clinics.Add(new Clincic.DataAccesslayer.Clinic
+                    {
+                        clinicName = clinic.clinicName,
+                        numberOfDoctors = clinic.numberOfDoctors,
+                        isOpen = clinic.isOpen,
+                        location = clinic.location
+                    });
+                }
+
+                _context.Clinic.AddRange(clinics);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
+

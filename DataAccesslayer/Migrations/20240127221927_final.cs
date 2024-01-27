80e051521fc6a4ada7ab92699b6849aa5136e2ea
karim <karim@DESKTOP-KM53IJE>
2024-01-28 00:33:04 +0200

    end

--- DataAccesslayer/Migrations/20240127221927_final.cs
+++ DataAccesslayer/Migrations/20240127221927_final.cs
@@ -14,7 +14,7 @@ namespace Clinic.DataAccesslayer.Migrations
         }
 
         /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
+        protected override void Down(MigrationBuilder  migrationBuilder)
         {
 
         }
